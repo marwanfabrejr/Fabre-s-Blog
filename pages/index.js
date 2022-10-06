541ef555657ef7bbd5c5981b3af954c4d79efeee
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2022-10-06 14:48:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const HomePage = (props) => {
   return (
     <Fragment>
       <Head>
-        <title>Marwan's' Blog</title>
+        <title>Marwan's Blog</title>
         <meta
           name='description'
           content='I post about programming and web development.'
